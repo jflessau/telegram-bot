567299e8abd24711591b4a52bb08ab0d8d2ae35a
jflessau <jan.jf@me.com>
2021-03-20 20:56:24 +0100

    add ChatMemberUpdated to UpdateKind

--- raw/src/types/mod.rs
+++ raw/src/types/mod.rs
@@ -1,6 +1,7 @@
 pub mod callback_query;
 pub mod chat;
 pub mod chat_member;
+pub mod chat_member_updated;
 pub mod inline_query;
 pub mod inline_query_result;
 pub mod input_file;
@@ -15,6 +16,7 @@ pub mod update;
 pub use self::callback_query::*;
 pub use self::chat::*;
 pub use self::chat_member::*;
+pub use self::chat_member_updated::*;
 pub use self::inline_query::*;
 pub use self::inline_query_result::*;
 pub use self::input_file::*;
